197a2d272bc5381a9161cdb53cfd39da681e0cb9
llwyd <tom@llwyd.io>
2018-07-04 16:44:23 +0100

    progress on implementing i2c master read

--- tempRead/temp.s
+++ tempRead/temp.s
@@ -15,9 +15,12 @@
 _start:
 	.word STACK_TOP, start
 start:
-	ldr r0, = 0x40023800	@ Address for RCC Clock Enable for GPIO A
+	/*Default clock = 16MHz*/
+	/* Configure and Setup LD2 located at PA5*/
+	ldr r0, = 0x40023800	@ Address for RCC Clock Enable for GPIO A and B
 	ldr r1, [r0, #0x30]	@ Load with Offset
-	orr r1, #1		@ Enable Port A
+	orr r1, #1		@ Enable GPIO A
+	orr r1, #2		@ Enable GPIO B
 	str r1, [r0, #0x30]	@ Store value
 
 	ldr r0, = 0x40020000 	@ Load Pin Mode Select Register GPIO A
@@ -25,8 +28,36 @@ start:
 	bfc r1, #10, #2  	@ Clear bits for PA5
 	orr r1, #0x400  	@ Set bits for Output
 	str r1, [r0, #0x00] 	@ Store value
+	/* Configure I2C*/
+	/*PB8=SCL*/
+	/*PB9=SDA*/
 
+	/*Set Alternative Functions*/
+        ldr r0, = 0x40020400    @ GPIO B base register
+        ldr r1, [r0, #0x00]     @ Load Pin Mode Select Register
+        bfc r1, #16, #4         @ Clear bits for PB8 and PB9
+        orr r1, #0xA0000        @ Set bits for Alternative Function (0b10)
+        str r1, [r0, #0x00]     @ Store value
+	/*Set Open Drain*/
+        ldr r1, [r0, #0x04]     @ Load output type register
+        orr r1, #256            @ Open Drain for PB8
+        orr r1, #512            @ Open Drain for PB9
+        str r1, [r0, #0x04]     @ Store value
+	/*Specify Alternate Function*/
+	/*I2C = AF04*/
+	ldr r1, [r0, #0x24]	@ Load AF High Register
+	orr r1, #0x44		@ AF04 for PB8 and 9
+	str r1, [r0, #0x24]	@ Store value
+	/*I2C Clock enable*/
+	ldr r0, = 0x40023800    @ Base Address for RCC
+	ldr r1, [r0, #0x40]     @ Load with Offset
+        orr r1, #0x200000       @ AF04 for PB8 and 9
+        str r1, [r0, #0x40]     @ Store value
 
+	/*I2C base address=0x40005400*/
+	/*Configure I2C1 as master mode*/
+
+	ldr r0, = 0x40020000    @ Load GPIO A base register for LED
 val:
 	MOV r2, #0x20 		@ Starting LED value (PA5)
 	MOV r1, #0x00000000
