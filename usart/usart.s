391f6a20f9dce644a4e19eb29c0f6dc1ea1f676d
llwyd <tom@llwyd.io>
2018-07-16 17:15:39 +0100

    basic Tx of single character

--- usart/usart.s
+++ usart/usart.s
@@ -31,21 +31,43 @@ start:
 	ldr r1, [r0,#0x20]	@ specify alternate function
 	orr r1, #0x7700		@ AF07
 	str r1, [r0,#0x20]	@ Store
+	/* Turn on USART2 peripheral*/
+        ldr r0, = 0x40023800    @ Base Address for RCC
+        ldr r1, [r0, #0x40]     @ Load with Offset
+        orr r1, #0x20000        @ Enable USART2
+        str r1, [r0, #0x40]     @ Store value
 	/*Configure USART2*/
 	/*Tx=PA2*/
 	/*Rx=PA3*/
 	/*Base address=0x40004400*/
+        ldr r0, = 0x40004400    @ Base Address for USART2
+	/*set baudrate*/
+	/* 115200 */
+        ldr r1, [r0, #0x08]     @ Load Baud Register
+        mov r2, #0x8		@Mantissa
+	lsl r2,#0x04		@shift right 4
+	orr r2, #0xb		@Add fraction
+        str r2, [r0, #0x08]     @ Store value
 
+        ldr r1, [r0, #0x0C]     @ Load Control Register 1
+        orr r1, #0x2000         @ Enable Tx and UART
+        orr r1, #0x08
+	str r1, [r0, #0x0C]     @ Store value
+write:
+	MOV r3,#0x54		@ Move letter 'T' to r3
+	str r3,[r0,#0x04]	@ Store in data register
+wait:
+	ldr r4,[r0, #0x00]	@Load status register
+	and r4,#0x40		@ bit mask
+	cmp r4,#0x40
+	bne wait
+	b write
 
-	MOV r1, #0x20		@ Initial value for PA5
-loop:
-	eors r1,#0x20		@ XOR with 0x20
-	str r1, [r0, #0x14] 	@ Store with offset
-	mov r3, #0x100000 	@ Arbitrary Delay
+	mov r3, #0x500000
 delay:
 	subs r3,1 		@ Substract 1 from delay
 	bne delay 		@ When zero reset
-	b loop			@ start loop again
+	b write			@ start loop again
 deadloop:
 	b	deadloop
 .end
